56f422878c3a9bb0dc22e1f398a3702fb7c65337
adrianoscibilia <adrscbl@gmail.com>
2023-06-12 11:13:02 +0200

    narmax trained

--- narmax.py
+++ narmax.py
@@ -10,11 +10,13 @@ from sysidentpy.utils.display_results import results
 from sysidentpy.utils.plotting import plot_residues_correlation, plot_results
 from sysidentpy.residues.residues_correlation import compute_residues_autocorrelation, compute_cross_correlation
 
-MODEL_FEATURES = 'narmax_poly2_PROVA'
+MODEL_FEATURES = 'narmax_poly5_0906'
 FILE_NAME = MODEL_FEATURES + '.pkl'
 FILE_NAME_ERROR = MODEL_FEATURES + '_error_' + '.pkl'
-TRAIN = True
-TRAIN_STOP = 10
+TRAIN = False
+TRAIN_STOP = 400
+EPOCHS = 100
+LOG_INTERVAL = 10
 
 data = pd.read_pickle('dataset_error_force_del_norm_1705.pkl')
 dataframe = pd.DataFrame(data)
@@ -31,45 +33,55 @@ for i in range(0, len(X.values)):
     ydata[i, :] = Y.values[i]["data"]
 
 X_train, X_tmp, Y_train, Y_tmp = train_test_split(np.transpose(xdata), np.transpose(ydata), test_size=0.2, shuffle=False)
-X_val, X_test, Y_val, Y_test = train_test_split(X_tmp, Y_tmp, test_size=0.5, shuffle=False)
-# x_id = X_train
-# y_id = Y_train
-# x_val = X_test
-# y_val = Y_test
+# X_val, X_test, Y_val, Y_test = train_test_split(X_tmp, Y_tmp, test_size=0.5, shuffle=False)
+X_test = X_tmp
+Y_test = Y_tmp
 
 if TRAIN:
     basis_function = Polynomial(degree=5)
+
+    xlag = 2  # [1, 2, 3, 4]
+    ylag = 2  # [1, 2, 3, 4]
 
     model = FROLS(
         order_selection=True,
         n_info_values=50,
         extended_least_squares=False,
-        ylag=2, xlag=2,
-        info_criteria='aic',
-        estimator='least_squares',
+        ylag=ylag, xlag=xlag,
+        info_criteria='bic',
+        estimator='recursive_least_squares',
         basis_function=basis_function,
         model_type='NARMAX'
     )
 
-    narmax = []
-    errors = []
+    # x_id = X_train[:, 0:3]
+    # y_id = Y_train[:, 0:3]
+    # model.fit(X=x_id, y=y_id)
+    # pickle.dump(model, open(FILE_NAME, 'wb'))
+
+    # narmax = []
+    # xi_list = []
+    # errors = []
     for i in range(0, TRAIN_STOP):
+        # i = np.random.randint(low=0, high=TRAIN_STOP)
         x_id = X_train[:, i].reshape(-1, 1)
         y_id = Y_train[:, i].reshape(-1, 1)
-        x_val = X_val[:, i].reshape(-1, 1)
-        y_val = Y_val[:, i].reshape(-1, 1)
-        narmax.append(model.fit(X=x_id, y=y_id))
-        y_hat_val = model.predict(X=x_val, y=y_val)
-        err_i = root_relative_squared_error(y_val, y_hat_val)
-        errors.append(err_i)
-        if i % 10 == 0:
+        # x_val = X_val[:, i].reshape(-1, 1)
+        # y_val = Y_val[:, i].reshape(-1, 1)
+        model = model.fit(X=x_id, y=y_id)
+        # narmax.append(mod_code)
+        # model.fit(X=x_id, y=y_id)
+        # y_hat_val = model.predict(X=x_val, y=y_val)
+        # err_i = root_relative_squared_error(y_val, y_hat_val)
+        # errors.append(err_i)
+        if i % LOG_INTERVAL == 0:
             print("progress: ", i)
 
-    pickle.dump(narmax, open(FILE_NAME, 'wb'))
-    pickle.dump(errors, open(FILE_NAME_ERROR,'wb'))
+    pickle.dump(model, open(FILE_NAME, 'wb'))
+    # pickle.dump(errors, open(FILE_NAME_ERROR,'wb'))
 if not TRAIN:
-    narmax = pickle.load(open(FILE_NAME, 'rb'))
-    errors = pickle.load(open(FILE_NAME_ERROR, 'rb'))
+    model = pickle.load(open(FILE_NAME, 'rb'))
+    # errors = pickle.load(open(FILE_NAME_ERROR, 'rb'))
 # r = pd.DataFrame(
 #     results(
 #         model.final_model, model.theta, model.err,
@@ -78,17 +90,18 @@ if not TRAIN:
 #     columns=['Regressors', 'Parameters', 'ERR'])
 
 
-chosen_idx = np.argmin(np.array(errors))
-chosen_model = narmax[chosen_idx]
-print("chosen model is number: ", chosen_idx)
+# chosen_idx = np.argmin(np.array(errors))
+chosen_model = model  # [TRAIN_STOP-1]
+# print("chosen model is number: ", chosen_idx)
 
-rand_idx = np.random.randint(low=0, high=TRAIN_STOP)
+rand_idx = 14  # np.random.randint(low=0, high=TRAIN_STOP-1)
+print("test idx: ", rand_idx)
 x_test = X_test[:, rand_idx].reshape(-1, 1)
 y_test = Y_test[:, rand_idx].reshape(-1, 1)
 x_plt = np.transpose(xdata)[:, rand_idx].reshape(-1, 1)
 y_plt = np.transpose(ydata)[:, rand_idx].reshape(-1, 1)
 y_hat_test = chosen_model.predict(X=x_test, y=y_test)
-y_hat =  chosen_model.predict(X=x_plt, y=y_plt)
+y_hat = chosen_model.predict(X=x_plt, y=y_plt)
 rrse = root_relative_squared_error(y_test, y_hat_test)
 
 time = np.linspace(start=0, stop=60, num=7500)
@@ -100,9 +113,30 @@ plt.ylabel('force')
 plt.legend(['truth', 'prediction'])
 plt.grid()
 
+rand_idx = np.random.randint(low=0, high=TRAIN_STOP-1)
+print("test idx: ", rand_idx)
+x_test = X_test[:, rand_idx].reshape(-1, 1)
+y_test = Y_test[:, rand_idx].reshape(-1, 1)
+x_plt = np.transpose(xdata)[:, rand_idx].reshape(-1, 1)
+y_plt = np.transpose(ydata)[:, rand_idx].reshape(-1, 1)
+y_hat_test = chosen_model.predict(X=x_test, y=y_test)
+y_hat = chosen_model.predict(X=x_plt, y=y_plt)
+rrse = root_relative_squared_error(y_test, y_hat_test)
+
+time = np.linspace(start=0, stop=60, num=7500)
+plt.figure(2)
+plt.plot(time, y_plt)
+plt.plot(time, y_hat)
+plt.xlabel('time')
+plt.ylabel('force')
+plt.legend(['truth', 'prediction'])
+plt.grid()
+
+plt.show()
+
 # plot_results(y=y_test, yhat = y_hat, n=1000)
-ee = compute_residues_autocorrelation(y_test, y_hat_test)
-plot_residues_correlation(data=ee, title="Residues Autocorrelation", ylabel="$e^2$")
-x1e = compute_cross_correlation(y_test, y_hat_test, x_test[:, 0])
-plot_residues_correlation(data=x1e, title="Residues Cross-correlation", ylabel="$x_1e$")
+# ee = compute_residues_autocorrelation(y_test, y_hat_test)
+# plot_residues_correlation(data=ee, title="Residues Autocorrelation", ylabel="$e^2$")
+# x1e = compute_cross_correlation(y_test, y_hat_test, x_test[:, 0])
+# plot_residues_correlation(data=x1e, title="Residues Cross-correlation", ylabel="$x_1e$")
 
